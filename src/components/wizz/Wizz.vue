b05a50fa77d2f46f32d2e2caf79e912af238866f
Omar Jbara <omar.jbara2@gmail.com>
2017-09-29 10:13:36 +0200

    Changed wizz animation based on selected theme

--- src/components/wizz/Wizz.vue
+++ src/components/wizz/Wizz.vue
@@ -6,11 +6,20 @@
 
   @import "../../styles/utils/_palette.scss";
 
-  @keyframes headShake {
+  .headShake-dark {
+    animation-name: headShakeDark;
+  }
+
+  .headShake-light {
+    animation-name: headShakeLight;
+
+  }
+
+  @keyframes headShakeDark {
     0% {
       -webkit-transform: translateX(0);
       transform: translateX(0);
-      background: hsl(0, 100%, 100%);
+      background: $shark_grey
     }
 
     6.5% {
@@ -54,7 +63,58 @@
     100% {
       -webkit-transform: translateX(0);
       transform: translateX(0);
-      background: $shark_dark_grey
+      background: $shark_grey
+    }
+  }
+  @keyframes headShakeLight {
+    0% {
+      -webkit-transform: translateX(0);
+      transform: translateX(0);
+      background: $whitesmoke
+    }
+
+    6.5% {
+      -webkit-transform: translateX(-12px) rotateY(-19deg);
+      transform: translateX(-12px) rotateY(-19deg);
+      background: $pale_green;
+
+    }
+
+    18.5% {
+      -webkit-transform: translateX(10px) rotateY(14deg);
+      transform: translateX(10px) rotateY(14deg);
+      background: $blue;
+
+    }
+
+    31.5% {
+      -webkit-transform: translateX(-6px) rotateY(-10deg);
+      transform: translateX(-6px) rotateY(-10deg);
+      background: $violet;
+
+    }
+
+    43.5% {
+      -webkit-transform: translateX(4px) rotateY(6deg);
+      transform: translateX(4px) rotateY(6deg);
+      background: $yellow;
+
+    }
+
+    60% {
+      -webkit-transform: translateX(4px) rotateY(6deg);
+      transform: translateX(4px) rotateY(6deg);
+      background: $rose;
+    }
+
+    80% {
+      background: $red;
+    }
+
+    100% {
+      -webkit-transform: translateX(0);
+      transform: translateX(0);
+      background: $whitesmoke
     }
   }
 </style>
@@ -67,7 +127,7 @@
     name: 'wizz',
     mounted() {
       let body = document.body;
-      body.classList.add("animated", "headShake");
+      body.classList.add("animated", "headShake-" + appStore.state.theme);
       body.addEventListener("animationend", (e) => this.onAnimationEnd(e));
       body.addEventListener("webkitAnimationEnd", (e) => this.onAnimationEnd(e));
       body.addEventListener("mozAnimationEnd", (e) => this.onAnimationEnd(e));
@@ -76,7 +136,7 @@
     },
     methods: {
       onAnimationEnd(e) {
-        e.target.classList.remove("animated", "headShake");
+        e.target.classList.remove("animated", "headShake", "headShake-light", "headShake-dark");
         appStore.setWizzing(false);
       }
     }
